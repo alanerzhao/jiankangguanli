9ab95b37f416ea0bab4c80414344801b531705c8
aifunlab <aaa>
2026-03-20 16:26:28 +0800

    Configure Supabase connection

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.HEALTH_APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  supabaseAuthEmail: "",
+  supabaseUrl: "https://deywovnfwogbejauhitd.supabase.co",
+  supabaseAnonKey: "sb_publishable_3E1T6WpcRE25UzucL836pg_XAZCrpMj",
+  supabaseAuthEmail: "alanerzhao@gmail.com",
 };
